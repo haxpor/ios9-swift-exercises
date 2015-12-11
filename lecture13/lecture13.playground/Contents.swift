791a5fa8e13bab3a20931c924928bdf91d42e9cc
Wasin Thonkaew <haxpor@gmail.com>
2015-12-11 13:35:27 +0700

    Add exercise of lecture 13

---
+++ lecture13/lecture13.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+// Each line corresponds to each requirement
+// note: there's a typo on requriement 4. in the course, it's 75 not 45
+
+import UIKit
+
+var myVar1 = 15
+var myVar2 = 5
+var sum = myVar1 + myVar2
+var multiplication = myVar1 * myVar2
+var myVar5 = myVar1 - myVar2
+var myVar6 = myVar1 / myVar2
+
+var anyName1 = 1, anyName2 = 2, anyName = 3
+let kAnyName1 = 1, kAnyName2 = 2, kAnyName = 3
+
+var myDouble : Double
+var myInt : Int = 423;
+var str : String = "Hello"
+var world : String = "World"
+
+var concat = str + world
+concat = "\(str) \(world)"
