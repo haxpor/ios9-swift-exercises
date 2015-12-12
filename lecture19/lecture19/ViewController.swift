81796cfb824dc00d868921f2b718b44b730fe425
Wasin Thonkaew <haxpor@gmail.com>
2015-12-12 15:30:37 +0700

    Add exercise in lecture19

---
+++ lecture19/lecture19/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lecture19
+//
+//  Created by Wasin Thonkaew on 12/12/15.
+//  Copyright © 2015 Wasin Thonkaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
